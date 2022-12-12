99469774be5a9f24dde6b038230793e64c2102f7
Allison Bergstedt <bergstedt1@gmail.com>
2022-12-12 08:52:23 -0500

    hero message update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -16,7 +16,7 @@ const Hero = () => {
                 <div className="px-4 py-5 text-center">
                     <h6 className="sub-title">AT TOP TIER</h6>
                     <h2 className="title">WE GUARANTEE ONLY THE 
-                    <strong>BEST STONEWORK</strong></h2>
+                    <strong> BEST STONEWORK</strong></h2>
                 <a className="btn radius-sm mx-2 my-1" href="tel:5678058719">Call Us</a>
                 <a className="btn radius-sm mx-2 my-1 white" href="#" onClick={handleClick}>Get a Quote</a>
                 </div>
